82b96405ee5929e115f6768a4153b8f85ec7f8e1
Xu Liu <liuxugt@gmail.com>
2019-01-29 15:57:38 -0500

    change layout and switch in note_list and view_note

--- app/app/src/main/java/com/example/myapplication/activity_view_note.java
+++ app/app/src/main/java/com/example/myapplication/activity_view_note.java
@@ -1,7 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class activity_view_note extends AppCompatActivity {
 
@@ -10,4 +12,19 @@ public class activity_view_note extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_note);
     }
+
+    public void save(View view){
+        /*Save the result on the firebase cloud*/
+
+        /*-------------------------------------*/
+
+        Intent i = new Intent(activity_view_note.this, activity_note_list.class);
+        startActivityForResult(i, 0);
+    }
+
+    public void discard(View view){
+        Intent i = new Intent(activity_view_note.this, activity_note_list.class);
+        startActivityForResult(i, 0);
+    }
+
 }
