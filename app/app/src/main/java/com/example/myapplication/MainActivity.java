ddaeab56f903bf96aa2dada7f6e834660d9367d1
Xu Liu <liuxugt@gmail.com>
2019-01-28 20:35:06 -0500

    layout for register screen

--- app/app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /** Called when the user taps the register button**/
+    public void register(View view){
+        TextView txt = (TextView) findViewById(R.id.reg_btn);
+        txt.setText("Submitted");
+    }
+
 }
